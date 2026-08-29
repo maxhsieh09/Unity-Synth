b1abad1e34aac6ab079a6a58b7c4a8799e32b0cb
maxhsieh09 <maxhsieh09@gmail.com>
2026-08-29 15:18:49 +0800

    Add a pick that follows the mouse

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    public bool hideCursor = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Cursor.visible = !hideCursor;
+        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = transform.position.z;
+        transform.position = mousePosition;
+    }
+}
